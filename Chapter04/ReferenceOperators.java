5af79e8dbacc72d998987185f9bc8691445f7fb4
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-09-19 17:41:14 +0900

    Update ReferenceOperators.java

--- Chapter04/ReferenceOperators.java
+++ Chapter04/ReferenceOperators.java
@@ -10,17 +10,20 @@ public class ReferenceOperators
 		String blue = new String( "blue" );
 		String red = new String( "red" );
 		System.out.println( "  color = green" );
+		
 		// 첫째 == : 참조값 비교
 		// 예) (String객체01)와 (String객체02)을 비교하면 거짓
 		System.out.print( "( color == green ) = " + ( color == green ) + ",     " );
 		System.out.print( "( color == blue ) = " + ( color == blue ) + ",      "  );
 		System.out.print( "( color == red ) = " + ( color == red ) + "\n" );
-		// 둘째 equals : 참조한 객체에 저장된 값이 같은지 비교
+		
+		// 둘째 equals : 참조한 객체에 저장된 값이 같은지 비교하고 boolean 값으로 반환
 		// 예) 문자열 'green'과 'green'을 비교하면 참
 		System.out.print( "  color.equals( green ) = " + color.equals( green ) + ", "  );
 		System.out.print( "  color.equals( blue ) = " + color.equals( blue ) + ", "  );
 		System.out.print( "  color.equals( red ) = " + color.equals( red )  + "\n" );
-		// 셋째 compareTo : 참조한 객체에 저장된 값을
+		
+		// 셋째 compareTo : 참조한 객체에 저장된 값을 비교하고 그 차이를 int 값으로 반환
 		// 예) 'g'-'b'= 5, 'g'-'r'= -11, 'g'-'g'= 0, 'r'-'r'= 0, 'e'-'e'= 0, 'n'-'n'= 0
 		System.out.print( "  color.compareTo( green ) = " + color.compareTo( green )  + ", " );
 		System.out.print( "  color.compareTo( blue ) = " + color.compareTo( blue ) + ",  "  );
