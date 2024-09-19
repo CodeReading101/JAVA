c9af5d00f04d8b8a2bfa62d799e1d12440529993
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-09-19 17:37:36 +0900

    Update Score.java

--- Chapter03/Score.java
+++ Chapter03/Score.java
@@ -9,6 +9,7 @@ public class Score
 		Scanner scan = new Scanner( System.in );
 		int korean = 0, english = 0, mathematics = 0;
 		double average = 0.0;
+		
 		// 과목별 점수를 입력
 		System.out.print( "0점 ~ 100점 사이의 국어 점수를 입력하세요: " );
 		korean = scan.nextInt();
@@ -16,8 +17,10 @@ public class Score
 		english = scan.nextInt();
 		System.out.print( "0점 ~ 100점 사이의 수학 점수를 입력하세요: " );
 		mathematics = scan.nextInt();
+		
 		// 평균 점수를 계산
 		average = (double)( korean + english + mathematics ) / 3;
+		
 		// 과목별 점수 및 평균을 출력
 		System.out.printf( "국어 %d점, 영어 %d점, 수학 %d점의 평균은 %.2f점입니다.",
 							korean, english, mathematics, average );
