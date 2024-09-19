0ae9edefbbf051390427fd24d5ed115f8be3bfd1
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-09-19 17:41:45 +0900

    Update Time.java

--- Chapter04/Time.java
+++ Chapter04/Time.java
@@ -8,13 +8,16 @@ public class Time
 		Scanner scan = new Scanner( System.in );
 		int totalSeconds = 0;
 		int hours = 0, minutes = 0, seconds = 0;
+		
 		// 사용자에게 전체 초를 입력받기
 		System.out.print( "초를 입력하세요: " );
 		totalSeconds = scan.nextInt();
+		
 		// 전체 초의 시, 분, 초를 계산
 		hours  = totalSeconds / ( 60 * 60 );
 		minutes = ( totalSeconds % ( 60 * 60 ) ) / 60;
 		seconds = totalSeconds % 60;
+		
 		// 시, 분, 초를 출력
 		System.out.print( totalSeconds + "초는 " + hours + "시간 " + minutes + "분 " + seconds + "초입니다." );
 	}
