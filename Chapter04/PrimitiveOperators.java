d0ac1cb9968c11eaa51c518cb738b0ced391ddd2
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-09-19 17:38:17 +0900

    Update PrimitiveOperators.java

--- Chapter04/PrimitiveOperators.java
+++ Chapter04/PrimitiveOperators.java
@@ -11,6 +11,7 @@ public class PrimitiveOperators
 		System.out.println( " 2 * 3 = " + ( 2 * 3 ) );
 		System.out.println( "10 / 3 = " + (10 / 3 ) );
 		System.out.println( "10 % 3 = " + (10 % 3 ) + "\n" );
+		
 		// 둘째 비교연산자 실습 및 출력: 1(참), 0(거짓)
 		System.out.println( "5 == 5 = " + ( 5 == 5 ) + ",   3 == 7 = " + ( 3 == 7 ) );
 		System.out.println( "9 != 2 = " + ( 9 != 2 ) + ",   8 != 8 = " + ( 8 != 8 ) );
@@ -18,6 +19,7 @@ public class PrimitiveOperators
 		System.out.println( "7 >  2 = " + ( 7 > 2 ) + ",   4 >  5 = " + ( 4 > 5 ) );
 		System.out.println( "4 <= 5 = " + ( 4 <= 5 ) + ",   7 <= 2 = " + ( 7 <= 2 ) );
 		System.out.println( "7 >= 2 = " + ( 7 >= 2 ) + ",   4 >= 5 = " + ( 4 >= 5 ) + "\n" );
+		
 		// 셋째 논리연산자 실습 및 출력: 1(참), 0(거짓)
 		System.out.println( "  !false   = " + !false + ",	 !true   = " + !true );
 		System.out.println( "false && false = " + ( false && false ) + ",   false && true = " + ( false && true ) + ",   true && false = " + ( true && false ) + ",   true && true = " + ( true && true ) );
