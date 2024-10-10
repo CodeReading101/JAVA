442d34127ea29d87b3a7a8b3e97d05c09e48427b
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-10-10 19:39:28 +0900

    Add files via upload

---
+++ Chapter09/Month.java
@@ -0,0 +1,17 @@
+// JAVA 프로그래밍 - https://codereading101.github.io/JAVA/
+// 소스파일 - https://github.com/CodeReading101/JAVA/blob/main/Chapter09/Month.java
+
+import java.util.Scanner;
+public class Month
+{
+	public static void main( String[] args ) {
+		Scanner scan = new Scanner( System.in );
+		// 달을 입력
+		System.out.print( "1월 ~ 12월 사이의 월을 입력하세요: " );
+		int month = scan.nextInt();
+		// 입력받은 달의 영어이름을 출력
+		String[] monthName = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",  "December" };
+		System.out.print( month + "월은 " + monthName[ month - 1 ] + "입니다" );
+	}
+}
+
---
+++ Chapter10/VectorMain.java
@@ -0,0 +1,40 @@
+// JAVA 프로그래밍 - https://codereading101.github.io/JAVA/
+// 소스파일 - https://github.com/CodeReading101/JAVA/blob/main/Chapter10/VectorMain.java
+
+import java.util.Vector;
+public class VectorMain
+{
+	public static void main( String[] args ) {
+		// 벡터 생성
+		Vector<String> list = new Vector<String>( 3 );
+		// 벡터에 객체를 추가
+		list.add( "벡터" );
+		list.add( "1" );
+		list.add( "2.5" );
+		list.add( 0, "Collection" );
+		list.add( 2, null );
+		// 벡터에서 객체를 제거
+		list.remove( 0 );
+		list.remove( 1 );
+		// 벡터의 객체를 출력
+		for( int i = 0; i < list.size(); i++ ) {
+			System.out.println( i + ") " + list.get(i) );
+		}
+		System.out.println( "총객체수 = " + list.size() );
+		System.out.println( "공간크기 = " + list.capacity() );
+		// 벡터에서 객체를 확인
+		if( list.contains( "Collection" ) )
+			System.out.println( "Collection 포함" );
+		else
+			System.out.println( "Collection 미포함" );
+		if( list.contains( "2.5" ) )
+			System.out.println( "2.5 포함" );
+		else
+			System.out.println( "2.5 미포함" );
+		// 벡터를 비우기
+		list.clear();
+		if( list.isEmpty() )
+			System.out.println( "빈 리스트" );
+	}
+}
+
