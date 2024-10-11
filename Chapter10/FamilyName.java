9b2cfccfdc5167cd6177e31fe17139d60ac571a1
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-10-11 14:57:06 +0900

    Update FamilyName.java

--- Chapter10/FamilyName.java
+++ Chapter10/FamilyName.java
@@ -1,6 +1,6 @@
 // JAVA 프로그래밍 - https://codereading101.github.io/JAVA/
 // 소스파일 - https://github.com/CodeReading101/JAVA/blob/main/Chapter10/FamilyName.java
-
+import java.util.Scanner;
 
 public class FamilyName
 {
