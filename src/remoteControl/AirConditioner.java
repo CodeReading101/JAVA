7c98f7aece8dc30908523b8ebc95fc7514bd6be3
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-10-17 17:23:52 +0900

    Update AirConditioner.java

--- src/remoteControl/AirConditioner.java
+++ src/remoteControl/AirConditioner.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
  *
  */
 public class AirConditioner implements RemoteControl
-
 {
 	// 에어컨 전원, 온도, 풍량 변수
 	private boolean power;
@@ -21,8 +20,7 @@ public class AirConditioner implements RemoteControl
 	 * AirConditioner 생성자로 에어컨 상태 초기화
 	 * @return 생성자이므로 해당사항 없음
 	 */
-	public AirConditioner()
-	{
+	public AirConditioner() {
 		this.power = OFF;
 		this.temperature = 19;
 		this.windStrength = 5;
@@ -32,15 +30,12 @@ public class AirConditioner implements RemoteControl
 	 * 에어컨 리모컨에서 전원버튼 눌렀을 때 전원을 켜거나 끄기
 	 * @return 전원 상태
 	 */
-	public String clickPower()
-	{
-		if( this.power == OFF )
-		{
+	public String clickPower() {
+		if( this.power == OFF ) {
 			this.power = ON;
 			return "에어컨 전원을 켭니다";
 		}
-		else
-		{
+		else {
 			this.power = OFF;
 			return "에어컨 전원을 끕니다";
 		}
@@ -50,8 +45,7 @@ public class AirConditioner implements RemoteControl
 	 * 에어컨 리모컨에서 상(△) 버튼을 눌렀을 때 온도를 +1 이동
 	 * @return 온도 상태
 	 */
-	public String clickUp()
-	{
+	public String clickUp() {
 		return "현재 온도는 " + ( ++this.temperature ) + "도입니다";
 	}
 
@@ -59,8 +53,7 @@ public class AirConditioner implements RemoteControl
 	 * 에어컨 리모컨에서 하(▽) 버튼을 눌렀을 때 온도를 -1 이동
 	 * @return 온도 상태
 	 */
-	public String clickDown()
-	{
+	public String clickDown() {
 		return "현재 온도는 " + ( --this.temperature ) + "도입니다";
 	}
 
@@ -68,8 +61,7 @@ public class AirConditioner implements RemoteControl
 	 * 에어컨 리모컨에서 좌(◁) 버튼을 눌렀을 때 풍량을 -1 이동
 	 * @return 풍량 상태
 	 */
-	public String clickLeft()
-	{
+	public String clickLeft() {
 		return "현재 풍량은 " + ( --this.windStrength ) + "입니다";
 	}
 
@@ -77,8 +69,7 @@ public class AirConditioner implements RemoteControl
 	 * 에어컨 리모컨에서 우(▷) 버튼을 눌렀을 때 풍량을 +1 이동
 	 * @return 풍량 상태
 	 */
-	public String clickRight()
-	{
+	public String clickRight() {
 		return "현재 풍량은 " + ( ++this.windStrength ) + "입니다";
 	}
 }
