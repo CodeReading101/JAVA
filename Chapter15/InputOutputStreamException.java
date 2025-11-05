8d56137b00fa2d8c71815223d10487b82884f0c2
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2025-11-05 18:40:32 +0900

    Update InputOutputStreamException.java

--- Chapter15/InputOutputStreamException.java
+++ Chapter15/InputOutputStreamException.java
@@ -3,8 +3,7 @@
 
 import java.io.*;
 public class InputOutputStreamException {
-	public static void main( String[] args )
-	                                         throws IOException {
+	public static void main( String[] args ) throws IOException {
 		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
 		// 이름 입력
@@ -18,4 +17,5 @@ public class InputOutputStreamException {
 		br.close();
 	}
 }
+
 
